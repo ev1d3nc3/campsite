f44d2d95aa81090afd2261e85b7587bcffe1e72a
ev1d3nc3 <josh@version9.org>
2015-07-13 16:36:21 -0700

    fixed a few things

--- campsite/wsgi.py
+++ campsite/wsgi.py
@@ -7,11 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
+import os 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campsite.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campsite.settings")
-
 application = Cling(get_wsgi_application())
